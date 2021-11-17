444feccdcc5a2bb7920def4785cc298e4134d75f
nastka <slimakpokazrogi@gmail.com>
2021-11-17 20:04:16 +0100

    wersja 2

--- PLiki_moduly.py
+++ PLiki_moduly.py
@@ -1,14 +1,33 @@
-# Otwórz plik
-fo = open("text.txt", "r")
-print("Nazwa pliku: ", fo.name)
+# # Otwórz plik
+# fo = open("text.txt", "r")
+# print("Nazwa pliku: ", fo.name)
+#
+# line = fo.readline()
+# print("Czytaj linię: >" + line + "<")
+#
+# # Ponownie ustaw wskaźnik na początek
+# fo.seek(0, 1)
+# line = fo.readline()
+# print("Czytaj linię: >" + line + "<")
+#
+# # Zamknij otwarty plik
+# fo.close(
+#
+#
+# import os
+# os.system("dir")
+#
+# from time import sleep
+# print("Dobranoc")
+# sleep(20)
+# print("Dzień dobry")
 
-line = fo.readline()
-print("Czytaj linię: >" + line + "<")
+# import time
+# print(dir(time))
 
-# Ponownie ustaw wskaźnik na początek
-fo.seek(0, 0)
-line = fo.readline()
-print("Czytaj linię: >" + line + "<")
+# import math
+# print(math.sin(math.pi/2))
 
-# Zamknij otwarty plik
-fo.close()
+def lemon(l):
+    print("lemoniada nr ", l)
+
